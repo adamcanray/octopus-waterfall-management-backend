b3b0acb86b5df1d109da33e3f2eba7366de43915
adamcanray <adamcool180@gmail.com>
2021-04-26 20:05:16 +0700

    setup some cors configs

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true});
 var db = mongoose.connection;
 // Configure cors
-app.use(cors())
+app.use(cors({credentials: true, origin: true}))
 
 // Added check for DB connection
 if(!db)
